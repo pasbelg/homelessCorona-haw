42a8b2284077d0d9473c68637ee348b4acdf47ca
pascal-PC <pascal.belger7@gmail.com>
2020-11-07 18:22:02 +0100

    Bezüge zu Config und Funktions Dateien gesetzt; Kommentare für Formularerstellung

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require("functions/formFunctions.php");
-
+require('conf.php');
+require('functions/formFunctions.php');
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -11,6 +11,12 @@ require("functions/formFunctions.php");
   </head>
   <body>
     <form action="result.php" method="post">
+        <!--Bitte alle Metainformationen mit dem Namen meta- Kennzeichnen. Das ist für die Zuordung der Meta Infos nötig. -->
+        <label for="meta-name">Pseudonym oder Name</label>
+        <input type="text" id="meta-name" name="meta-name"><br>
+        <label for="meta-streetYears">Jahre auf der Straße</label>
+        <input type="number" id="meta-streetYears" name="meta-streetYears" min="1" max="100"><br>
+        <!--Die anzukreuzenden Felder werden über die CSV-Dateien in files/in/ generiert -->
         <?php genForm(csvToArray($fileC),csvToArray($fileQ));?>
       <p><input type="submit" /></p>
     </form>
