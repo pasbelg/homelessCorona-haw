950ac8d106c329fdbdd703e854ced82e512c8aaa
pascal-PC <pascal.belger7@gmail.com>
2020-12-19 15:49:54 +0100

    Simple Ausgabe für im Formular erfasste Daten erstellt

--- result.php
+++ result.php
@@ -3,4 +3,5 @@ require('conf.php');
 require('functions/formFunctions.php');
 $input = postToArray($_POST, csvToArray($fileC));
 print("<pre>".print_r($input,true)."</pre>");
+echo json_encode($input);
 ?>
