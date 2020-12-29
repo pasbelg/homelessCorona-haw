8f2eb24c7155860f0a58182879c4ebbe591bfed5
pascal-PC <pascal.belger7@gmail.com>
2020-12-29 17:46:12 +0100

    Ausgabe zum lesen der gespeicherten User Daten

--- result.php
+++ result.php
@@ -2,6 +2,11 @@
 require('conf.php');
 require('functions/formFunctions.php');
 $input = postToArray($_POST, csvToArray($fileC));
-print("<pre>".print_r($input,true)."</pre>");
-echo json_encode($input);
+saveEntries($input, $userData);
+#echo checkUserExistence($input['metaInfo']['meta-name'], $userData) ? 'true' : 'false';
+#saveEntrys($input, $outputPath.$input[])
+#print("<pre>".print_r($input,true)."</pre>");
+#echo json_encode($input);
+$json = readEntries('Pascal');
+print("<pre>".print_r($json,true)."</pre>");
 ?>
