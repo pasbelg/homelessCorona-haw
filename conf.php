3993ae722d0636cd2bdb2a8da91c356a43a07b87
pascal-PC <pascal.belger7@gmail.com>
2020-12-29 17:45:45 +0100

    Pfad zu User Daten hinzugefügt

--- conf.php
+++ conf.php
@@ -5,5 +5,7 @@ ini_set("display_errors", 1);
 # Root Pfad der Website
 $rootPath = __DIR__;
 # Pfad zu input Dateien
-$inputFiles = $rootPath . '/files/in/';
+$inputPath = $rootPath . '/files/in/';
+$outputPath = $rootPath . '/files/out/';
+$userData = $outputPath . '/userData/'
 ?>
