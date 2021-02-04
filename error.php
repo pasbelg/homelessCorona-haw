054191aadbc7d40b95f004eb3e90d01adc2f3a16
pascal-PC <pascal.belger7@gmail.com>
2021-02-04 11:40:47 +0100

    Errorpage hinzugefügt

---
+++ error.php
@@ -0,0 +1,19 @@
+<?php
+include('header.php');
+/*
+require('conf.php');
+require('functions/formFunctions.php');
+<!DOCTYPE html>
+    <html lang="de">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Was kostet die Straße?</title>
+        </head>
+        <body>*/
+?>
+
+        <p> Es gab einen Fehler bei der Verarbeitung deiner Daten. <a href="index.php">Hier</a> kannst du die Kosten auf der Straße erneut berechnen</p>
+
+        </body>
+    </html>
